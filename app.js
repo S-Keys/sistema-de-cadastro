cd7c3b15a55792e82de7e08173c6ac4e4bc25c53
Sérgio Chavesr <sergio.chaves@outlook.com>
2023-02-03 13:28:21 +0044

    Resolvendo problemas de ícones

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Acrescentando alteração via GitHub para visualisação de alterações no comando git pull no terminal")
-;)
+";)"
 console.log("Retornando para GitHub alteração baixad via pull e enviando ela mais esta alteração")
->:)
+">:)"
